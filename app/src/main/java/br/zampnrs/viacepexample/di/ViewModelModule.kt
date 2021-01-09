70a7c297f6f46c9690622ea8fc64083a983d3fbd
zampnrs <46901316+zampnrs@users.noreply.github.com>
2021-01-09 12:33:56 -0300

    Update ViewModelModule.kt

--- app/src/main/java/br/zampnrs/viacepexample/di/ViewModelModule.kt
+++ app/src/main/java/br/zampnrs/viacepexample/di/ViewModelModule.kt
@@ -1,9 +1,9 @@
 package br.zampnrs.viacepexample.di
 
-import br.zampnrs.viacepexample.module.home.viewmodel.MainViewModel
+import br.zampnrs.viacepexample.module.home.viewmodel.ContactViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModel { MainViewModel(get(), get()) }
+    viewModel { ContactViewModel(get(), get()) }
 }
